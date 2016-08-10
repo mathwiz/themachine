93cb8656d5e0289f951bf3921cf04402a6d543e6
mathwiz <mathwiz@gmail.com>
2016-08-09 19:46:24 -0500

    more examples

---
+++ nand2tetris/04/mult/Flip.asm
@@ -0,0 +1,26 @@
+//Flip.asm
+//Flips RAM[0] and RAM[1]
+
+// Example of variable
+// temp = R1
+// R1 = R0
+// R0 = temp
+
+@R1
+D=M
+@temp
+M=D	//temp=R1
+
+@R0
+D=M
+@R1
+M=D	//R1=R0
+
+@temp
+D=M
+@R0
+M=D	//R0=temp
+
+(END)
+@END
+0;JMP
