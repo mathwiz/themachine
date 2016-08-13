c5777ead1e6d59dee23699a763b2a53ea10c9646
mathwiz <mathwiz@gmail.com>
2016-08-12 19:47:15 -0500

    Fill

--- nand2tetris/04/fill/Fill.asm
+++ nand2tetris/04/fill/Fill.asm
@@ -9,3 +9,51 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(INIT)
+@SCREEN
+D=A
+@8192
+D=D+A
+@addr
+M=D	//addr=16384 + 256 * 512 (screen base address + all screen addresses)
+@laddr
+M=D
+
+(LOOP)
+	@addr
+	D=M
+	@SCREEN
+	D=D-A
+	@RESET
+	D;JLT
+
+	(SCAN)
+	@KBD
+	D=M
+	@CLEAR
+	D;JEQ
+	
+	@addr
+	A=M
+	M=-1
+	
+	(INC)
+	@addr
+	M=M-1
+	@LOOP
+	0;JMP
+
+(RESET)
+	@laddr
+	D=M
+	@addr
+	M=D
+	@SCAN
+	0;JMP
+	
+(CLEAR)
+	@addr
+	A=M
+	M=0
+	@INC
+	0;JMP
